3573cf44aa1b9c7834580fd44dace0ea40687425
Mark Nijhof <mark.nijhof@cre8ivethought.com>
2014-12-19 13:55:58 +0100

    added date

--- javascripts/time.js
+++ javascripts/time.js
@@ -13,6 +13,9 @@ setInterval(function() {
     var currentSeconds = getMsSinceMidnight(new Date()) / 1000;
     var R, G, B = 0;
 
+    setTime();
+    setDate();
+
     // 00:00:00
     // R 107 242
     // G 107 107
@@ -22,7 +25,6 @@ setInterval(function() {
         G = min;
         B = max;
         setBackgroundColor(R,G,B);
-        setTime();
         return
     }
 
@@ -35,7 +37,6 @@ setInterval(function() {
         G = min;
         B = max - Math.round(diff / (1 / (currentSeconds / 14400 - 1)));
         setBackgroundColor(R,G,B);
-        setTime();
         return
     }
 
@@ -49,7 +50,6 @@ setInterval(function() {
         G = min + Math.round(diff / (1 / (currentSeconds / 14400 - 2)));
         B = min;
         setBackgroundColor(R,G,B);
-        setTime();
         return
     }
 
@@ -62,7 +62,6 @@ setInterval(function() {
         G = max;
         B = min;
         setBackgroundColor(R,G,B);
-        setTime();
         return
     }
 
@@ -75,7 +74,6 @@ setInterval(function() {
         G = max;
         B = min + Math.round(diff / (1 / (currentSeconds / 14400 - 4)));
         setBackgroundColor(R,G,B);
-        setTime();
         return
     }
 
@@ -88,7 +86,6 @@ setInterval(function() {
         G = max - Math.round(diff / (1 / (currentSeconds / 14400 - 5)));
         B = max;
         setBackgroundColor(R,G,B);
-        setTime();
         return
     }
 
@@ -108,11 +105,12 @@ function checkTime(i) {
 
 function setTime() {
     var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    // add a zero in front of numbers<10
-    m = checkTime(m);
-    s = checkTime(s);
+    var h = checkTime(today.getHours());
+    var m = checkTime(today.getMinutes());
+    var s = checkTime(today.getSeconds());
     document.getElementById("time").innerHTML = h + ":" + m + ":" + s;
+}
+
+function setDate() {
+    document.getElementById("date").innerHTML = moment().format("dddd, MMMM Do YYYY");;
 }
